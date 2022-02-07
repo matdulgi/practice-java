8324de84ee5be941eb574727a7c88a6ce46238ed
yura <yrdog9494@gmail.com>
2022-02-07 19:44:56 +0900

    ClassClass - runtime referene

--- reflectionEx/ClassClass.java
+++ reflectionEx/ClassClass.java
@@ -1,7 +1,7 @@
 package reflectionEx;
 
 public class ClassClass {
-    public static void main(){
+    public static void main(String args[]){
         // .getClass() only valid for an instance
         // Class class1 = String.getClass();
         Class class1 = new String().getClass();
@@ -10,7 +10,19 @@ public class ClassClass {
         // Class class2 = new String().class;
         Class class2 = String.class;
 
-        
+        /*
+        try {
+            Class class3 = Class.forName("String");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        */
+
+        try {
+            Class class3 = Class.forName("java.lang.String");
+            System.out.println("class3 : " + class3.getName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
