4bce9512893a2fb82ae422b3bc091faf449cfa87
yura <yrdog9494@gmail.com>
2022-02-07 15:03:34 +0900

    BasicCastTest - test2

--- castingTest/BasicCastTest.java
+++ castingTest/BasicCastTest.java
@@ -1,9 +1,19 @@
 package castingTest;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class BasicCastTest{
     public static void main (String[] args){
         Parent parent = new Children();
         parent.test();
+        parent.parent();
+        //overrided method is applied
+        //but, cannot invoke children's own method
+        if (parent instanceof Children){
+            Children children = (Children)parent;
+            children.children();
+        }
+        //it needs to declare for children's type
     }
 
 }
@@ -12,6 +22,9 @@ class Parent{
     public void test(){
         System.out.println("it's parent");
     }
+    public void parent(){
+        System.out.println("parent");
+    }
 
 }
 
@@ -19,5 +32,8 @@ class Children extends Parent{
     @Override
     public void test(){
         System.out.println("it's children");
+    }
+    public void children(){
+        System.out.println("children");
     }
 }
